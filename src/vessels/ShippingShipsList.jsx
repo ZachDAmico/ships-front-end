7fdad28c2e39bc1f686f9e31c732442f8d28a5a3
Zach D'Amico <zachdamico90@gmail.com>
2023-10-20 15:11:25 -0500

    delete finished for ships

--- src/vessels/ShippingShipsList.jsx
+++ src/vessels/ShippingShipsList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getShippingShips } from "../fetches/shippingShipsFetch";
+import { deleteShip, getShippingShips } from "../fetches/shippingShipsFetch";
 export const ShippingShipList = () => {
   const [ships, setShips] = useState([]);
 
@@ -9,13 +9,30 @@ export const ShippingShipList = () => {
     });
   }, []);
 
+  const handleDelete = (id) => {
+    deleteShip(id).then(() => {
+      getShippingShips().then((shipArray) => {
+        setShips(shipArray);
+      });
+      window.alert("You sank my BattleShip!");
+    });
+  };
   return (
     <div>
       <h2>Ships</h2>
       {ships.map((ship) => {
         return (
           <div key={ship.id}>
+            {/* don't need to map haulers here because we expanded ships to have the hauler info in the back end so fetch just needs to be updated */}
             <div>name: {ship.name}</div>
+            <p>Hauler: {ship.hauler.name}</p>
+            <button
+              onClick={() => {
+                handleDelete(ship.id);
+              }}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
